f2c5e2780883b4161a98bace0dcacc9ba3c4c159
ahmedabdo547 <147623299+ahmedabdo547@users.noreply.github.com>
2025-12-11 00:57:20 +0200

    Add files via upload

---
+++ itl_phone/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Itl Phone',
+    'version': '1.0',
+    'summary': 'Brief description of the module',
+    'description': '''
+        Detailed description of the module
+    ''',
+    'category': 'Uncategorized',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Ahmed Abdelhameed',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'web'],
+    'data': [
+        'views/itl_phone_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'itl_phone/static/src/components/**/*.js',
+            'itl_phone/static/src/components/**/*.xml',
+        ]
+    },
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
